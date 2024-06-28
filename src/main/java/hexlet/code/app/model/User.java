26954c6fa067bc28827d991e1d090239fca29291
algins <igors.algins@gmail.com>
2024-06-28 08:57:39 +0300

    feat: database

---
+++ src/main/java/hexlet/code/app/model/User.java
@@ -0,0 +1,53 @@
+package hexlet.code.app.model;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import java.time.LocalDate;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString(includeFieldNames = true, onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @ToString.Include
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @ToString.Include
+    private String firstName;
+
+    @ToString.Include
+    private String lastName;
+
+    @Column(unique = true)
+    @Email
+    @ToString.Include
+    private String email;
+
+    private String passwordDigest;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+}
